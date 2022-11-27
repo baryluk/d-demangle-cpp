1464b779401474061a4faa73b890890facf9fdfc
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2022-11-28 00:53:08 +0100

    Add --verbose mode to example and add more tests and hints

--- d-demangle-main.cc
+++ d-demangle-main.cc
@@ -6,10 +6,12 @@
 int main(int argc, char **argv) {
   bool return_types = true;
   bool function_attributes = true;
+  bool verbose = false;
 
   if (argc <= 1 || std::string(argv[1]) == "--help") {
     std::cerr << "Usage: " << argv[0]
-              << " [--help] [--no_return_type] [--no_function_attributes] "
+              << " [--help] [--verbose] [--no_return_type] "
+                 "[--no_function_attributes] "
                  "_Dsymbol _Dsymbol ..."
               << std::endl;
     return 1;
@@ -23,6 +25,13 @@ int main(int argc, char **argv) {
     if (std::string(argv[i]) == "--no_function_attributes") {
       function_attributes = false;
       continue;
+    }
+    if (std::string(argv[i]) == "--verbose") {
+      verbose = true;
+      continue;
+    }
+    if (verbose) {
+      std::cout << "Input: " << argv[i] << std::endl;
     }
     std::cout << demangle_d::demangle(argv[i], return_types,
                                       function_attributes)

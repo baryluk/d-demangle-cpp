619c8c2fef968de2c49898bd2fae9cad9c9f02bc
Witold Baryluk <witold.baryluk@gmail.com>
2022-11-29 23:15:04 +0000

    Add include guard

--- d-demangle.h
+++ d-demangle.h
@@ -1,3 +1,6 @@
+#ifndef DEMANGLE_D_H
+#define DEMANGLE_D_H
+
 #include <string>
 
 namespace demangle_d {
@@ -6,3 +9,5 @@ std::string demangle(const std::string &s, bool return_types = true,
                      bool function_attributes = true);
 
 }  // namespace demangle_d
+
+#endif  // DEMANGLE_D_H
